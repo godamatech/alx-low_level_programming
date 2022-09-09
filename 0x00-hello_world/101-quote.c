dd3252fadda3ef8d50404d5fc8a01011a3020f67
Godamatech <abdullahibello8944@gmail.com>
2022-09-08 18:39:25 -0700

    initial commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <unistd.h>
 
 /**
  * main - Entry point
@@ -10,11 +11,7 @@ int main(void)
 
 {
 
-	char str1[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-
-
-
-	write(2, str1, 59);
+	write(1,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
 	return (1);
 
