61cc3d4c77f16e493966e6b89cc1fe1f7bdca99e
Godamatech <abdullahibello8944@gmail.com>
2022-09-09 22:07:56 -0700

    initial commit

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -7,13 +7,14 @@
  *
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
 	char low;
 
 	for (low = 'a'; low <= 'z'; low++)
 		putchar(low);
-		putchar('\n);
+		putchar('\n');
 
 	return (0);
 }
