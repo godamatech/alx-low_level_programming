8c1d96a13e7a4634046b98d22f7ffc9f234876a9
Godamatech <abdullahibello8944@gmail.com>
2022-09-09 23:06:41 -0700

    initial commit

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - main Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int low;
+
+	for (low = 'z'; low >= 'a'; low--)
+	{
+		putchar(low);
+		putchar('\n');
+	}
+	return (0);
+}
