78557cf4d6bd7cfa087380464f2724b2b3af0330
Godamatech <abdullahibello8944@gmail.com>
2022-09-16 18:30:56 -0700

    intial commit

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_numbers
+ *
+ * Description:
+ *
+ * Return void
+ */
+
+void print_numbers(void)
+{
+	int number;
+	
+	for (number=0; number <= '9'; number++)
+	{
+		_putchar(number);
+	}
+	_putchar('\n');
+}
